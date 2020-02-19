6532336de4a7afa784647a3d4da1a06f2bf754e5
Secure77 <31564517+secure-77@users.noreply.github.com>
2020-02-19 17:35:19 +0100

    Create config.h

---
+++ Basic/config.h
@@ -0,0 +1,14 @@
+
+// wifi settings
+#define SSID "Wifi SSID"
+#define PASSWORD "Wifi Password"
+
+// email settings
+const char* smtp_server = "smtp_server";
+const int   smtp_port = 465;
+const char* smtp_login = "smtp_login";
+const char* smtp_password = "smtp_password";
+const int   smtp_timeout = 5000;
+const bool  smtp_ssl = true;
+const char* mail_from = "mail_from";
+const char* mail_to = "mail_to";
