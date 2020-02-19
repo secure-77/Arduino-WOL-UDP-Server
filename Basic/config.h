772f5e5573191b65a24784a26a2e7eef7927c463
Secure77 <31564517+secure-77@users.noreply.github.com>
2020-02-19 18:27:14 +0100

    fixed some typos

--- Basic/config.h
+++ Basic/config.h
@@ -2,12 +2,12 @@
 
 // wifi settings
 #define SSID "Wifi SSID"
-#define PASSWORD "Wifi Password"
+#define PASSWORD "Wifi password"
 IPAddress ip(192,168,1,5);   
 IPAddress gateway(192,168,1,1);   
 IPAddress subnet(255,255,255,0); 
 
-// Target Domain, local WOL IP, MAC and Broadcast Addres
+// target domain, local WOL IP, MAC and broadcast address
 const char* dynDNS = "yourDomain.dyndns.org";
 IPAddress serverIP (192, 168, 1, 10);
 byte mac[] = {0x2C, 0xFD, 0xA1, 0xB8, 0x9B, 0x2D};
@@ -23,14 +23,14 @@ const bool  smtp_ssl = true;
 const char* mail_from = "mail_from";
 const char* mail_to = "mail_to";
 
-// The server accepts connections on this port
+// UDP server port
 #define PORT 5444
 
-// code tries
+// max. false code tries
 int code_tries = 3;
 
-// Delay between WOL sending
+// delay between WOL sending
 byte senddelay = 10;
 
-// wake up retries
+// WOL send retries
 const int retryCount = 3;
